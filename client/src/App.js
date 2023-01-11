7c38fdea5740256b9ea12130f495e2fe3d4dae39
Dawa <sherpadawa01@outlook.com>
2023-01-11 21:55:14 +0545

    working on frontend part

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import AboutUs from './Screens/AboutUs';
+import NotFound from './Screens/NotFound';
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
